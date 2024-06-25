e94f53fea0e90a04b313e4709c2e1a482f5b7f34
aldinash <aldinashss@gmail.com>
2024-06-25 14:49:29 +0500

    resolved some issues

--- gitvis/cli.py
+++ gitvis/cli.py
@@ -4,13 +4,13 @@ from . import stats, print, scan, utils
 
 
 @click.command()
-@click.option("-d", "--directory", default=".", type=str, help="Target path to scan")
+@click.option("-d", default=".", type=str, help="Target path to scan")
 @click.option(
     "--email",
     default=utils.get_global_email("."),
     help="Email you want to show commits for",
 )
-def run(directory, email):
-    git_folders = scan.scan_git_folders(directory)
+def run(d, email):
+    git_folders = scan.scan_git_folders(d)
     commits = stats.process_repos(git_folders, email)
     print.print_table(commits)
--- gitvis/stats.py
+++ gitvis/stats.py
@@ -5,18 +5,19 @@ from typing import List
 
 def __fill_commits(path: str, email: str, commits):
     repo = git.Repo(path)
-    six_month_ago = datetime.now().date() - timedelta(days=180)
+    now = datetime.now().date()
+    six_month_ago = now - timedelta(days=180)
     for commit in repo.iter_commits():
         if commit.author.email != email:
             continue
-        commit_date = commit.committed_date
+        commit_date = datetime.utcfromtimestamp(commit.committed_date).date()
         if commit_date < six_month_ago:
             continue
         days_ago = (now - commit_date).days
         if days_ago not in commits:
             commits[days_ago] = 1
         else:
-            commit[days_ago] += 1
+            commits[days_ago] += 1
 
 
 def process_repos(repos: List[str], email: str):
