e1b1abf0e00b11fb36bfe75f6e45e2cf195978fe
aldinash <aldinashss@gmail.com>
2024-06-28 13:26:17 +0500

    added errors

--- gitvis/__init__.py
+++ gitvis/__init__.py
@@ -2,3 +2,11 @@ __app_name__ = "gitvis"
 __version__ = "0.1.0"
 
 from . import cli
+
+(SUCCESS, DIR_ERROR, EMAIL_ERROR, DIR_ERROR, DATE_ERROR) = range(5)
+
+ERRORS = {
+    DIR_ERROR: "Please provide correct directory",
+    EMAIL_ERROR: "Cannot find global email. Try to pass email manually",
+    DATE_ERROR: "Date Error. Make sure your provided a valid date",
+}
