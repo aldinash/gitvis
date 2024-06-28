85716e23b03f499cc749b89543f0e95d0e06be8c
aldinash <aldinashss@gmail.com>
2024-06-28 14:26:47 +0500

    added error support

--- gitvis/cli.py
+++ gitvis/cli.py
@@ -1,16 +1,26 @@
 import click
 
-from . import stats, print, scan, utils
+from . import print_m, stats, scan, utils
+from gitvis import DIR_ERROR, EMAIL_ERROR, ERRORS
 
 
 @click.command()
 @click.option("-d", default=".", type=str, help="Target path to scan")
 @click.option(
     "--email",
-    default=utils.get_global_email("."),
+    default=None,
+    # default=utils.get_global_email("."),
     help="Email you want to show commits for",
 )
 def run(d, email):
-    git_folders = scan.scan_git_folders(d)
+    if email == None:
+        email, error = utils.get_global_email(d)
+        if error == EMAIL_ERROR:
+            print(ERRORS[EMAIL_ERROR])
+            return
+    git_folders, error = scan.scan_git_folders(d)
+    if error == DIR_ERROR:
+        print(ERRORS[DIR_ERROR])
+        return
     commits = stats.process_repos(git_folders, email)
-    print.print_table(commits)
+    print_m.print_table(commits)
--- gitvis/utils.py
+++ gitvis/utils.py
@@ -1,6 +1,10 @@
 from git import Repo
+from gitvis import SUCCESS, EMAIL_ERROR
 
-def get_global_email(repo: str) -> str:
-    repo = Repo(repo)
-    git_config = repo.config_reader()
-    return git_config.get_value("user", "email")
+def get_global_email(repo):
+    try:
+        repo = Repo(repo)
+        git_config = repo.config_reader()
+        return git_config.get_value("user", "email"), SUCCESS
+    except:
+        return None, EMAIL_ERROR
