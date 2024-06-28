cd8e21838a4f47475daddd3d59050e10b2d178f0
aldinash <aldinashss@gmail.com>
2024-06-28 14:31:32 +0500

    removed main.py and added something

--- gitvis/utils.py
+++ gitvis/utils.py
@@ -5,6 +5,6 @@ def get_global_email(repo):
     try:
         repo = Repo(repo)
         git_config = repo.config_reader()
-        return git_config.get_value("user", "email"), SUCCESS
+        return git_config.get_value("user", "email"), SUCCESS   
     except:
         return None, EMAIL_ERROR
